4a607f9e33c8762833ec6aba16e72e17b3a43076
Anthony Miyoro <amiyoro@gmail.com>
2015-09-29 17:11:57 +0300

    Cut the views from the urls.py to the views.py

--- helloworld/helloworld/urls.py
+++ helloworld/helloworld/urls.py
@@ -14,10 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse("Hello World!")
 
 #from django.contrib import admin
 
