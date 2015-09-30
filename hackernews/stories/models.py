21a06e9c00490d9c59bd7da0139aba3690bb1de0
Anthony Miyoro <amiyoro@gmail.com>
2015-09-30 10:23:31 +0300

    Created a story model

--- hackernews/stories/models.py
+++ hackernews/stories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here..
+
+class Story(models.Model):
+    title
+    url
+    points
+    poster
+    created_at
+    updated_at
+
+https://github.com/anthonymiyoro/HackerNewsClone.git
