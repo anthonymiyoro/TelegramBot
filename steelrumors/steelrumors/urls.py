e797553857e247bb20224c974f93816e3ec1d7a3
Anthony Miyoro <amiyoro@gmail.com>
2015-11-07 13:42:00 +0300

    Created a homepage

--- steelrumors/steelrumors/urls.py
+++ steelrumors/steelrumors/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
+from links.views import LinkListView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', LinkListView.as_view(), name='home')
 ]
