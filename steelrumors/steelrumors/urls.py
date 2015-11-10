798c0688d90e240521ddc89b40940a92438e9e05
Anthony Miyoro <amiyoro@gmail.com>
2015-11-10 14:35:27 +0300

    Created login and logout urls

--- steelrumors/steelrumors/urls.py
+++ steelrumors/steelrumors/urls.py
@@ -20,5 +20,6 @@ from links.views import LinkListView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', LinkListView.as_view(), name='home')
+    url(r'^$', LinkListView.as_view(), name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'login.html'}, name="login"),
 ]
