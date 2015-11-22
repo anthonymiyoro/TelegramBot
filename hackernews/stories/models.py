6b85e83c5c4b69530695ddf0e68e3793b5e2a976
Anthony Miyoro <amiyoro@gmail.com>
2015-11-22 19:47:16 +0300

    Registered the previously created stories app.

--- hackernews/stories/models.py
+++ hackernews/stories/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Story(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
-    points = models.IntegerField()
+    points = models.IntegerField(default=0)
     moderator = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
