8d52c35834831248062a91f099ddd4cfa0b1e654
Anthony Miyoro <amiyoro@gmail.com>
2015-11-23 12:01:07 +0300

    Added sort by time feature

--- hackernews/stories/admin.py
+++ hackernews/stories/admin.py
@@ -5,6 +5,6 @@ from stories.models import Story
 
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'domain', 'moderator', 'created_at', 'updated_at')
-
+    list_filter = ('created_at', 'updated_at')
 admin.site.register(Story, StoryAdmin)
 
