d8cc8bdae1126a828554166a6b9481e8a9bf92a9
Anthony Miyoro <amiyoro@gmail.com>
2015-11-26 19:37:56 +0300

    Added listing of stories by created date in descending order

--- hackernews/stories/views.py
+++ hackernews/stories/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from stories.model import story
+
+def top_stories(top=180, consider=1000):
+    latest_stories = Story.objects.all().order_by('-created_at')[:consider]
 
 # Create your views here.
 def index(request):
     stories = top_stories(top=30)
-    response = ''
+    response = '''
+    <html>
+    <head>
+    <title>Stolen Rumors</title>
+    </head>
+    <body>
+     %s
+    </body>
+    </html>
+     ''' % '\n'.join(['<li>%s</li>' % story.title for story in stories])'
     return HttpResponse('The Main Page')
