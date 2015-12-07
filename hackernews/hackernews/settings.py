4974fa858230b2a4affed8388b8621f7e6a58026
Anthony Miyoro <amiyoro@gmail.com>
2015-12-07 11:50:50 +0300

    Replaced the db with a functioning one

--- hackernews/hackernews/settings.py
+++ hackernews/hackernews/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hackernews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join( 'static'),],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
