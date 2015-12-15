e95bf303cfb8049eb3fa31b7b0c06cf074598ad7
Anthony Miyoro <amiyoro@gmail.com>
2015-12-15 12:12:17 +0300

    Fixed unknown admin id bug

--- hackernews/stories/models.py
+++ hackernews/stories/models.py
@@ -1,13 +1,15 @@
-from django.db import models
 from urlparse import urlparse
+
+from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.core.urlresolvers import reverse
 
 class Story(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
-    points = models.IntegerField(default = 1)
-    moderator = models.ForeignKey(User)
+    points = models.IntegerField(default=1)
+    moderator = models.ForeignKey(User, related_name='moderated_stories')
+    voters = models.ManyToManyField(User, related_name='liked_stories')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,11 +17,15 @@ class Story(models.Model):
     def domain(self):
         return urlparse(self.url).netloc
 
+    def get_absolute_url(self):
+        return reverse('stories:story_detail', args=(self.id,))
+
     def __unicode__(self):
         return self.title
 
     class Meta:
         verbose_name_plural = "stories"
 
-
-
+class Comment(models.Model):
+    story = models.ForeignKey(Story, related_name='comments')
+    body = models.TextField()
